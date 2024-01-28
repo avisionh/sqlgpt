f77188249a23f079b380336ecae5103507e6f1a2
avisionh <a_vision@hotmail.co.uk>
2024-01-28 22:50:25 +0000

    feat: Call OpenAI API to get response to question
    This is so we can see how well it performs.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-from src.constants import SQLALCHEMY_URL
+from src.constants import SQLALCHEMY_URL, GPT_MODEL
 
 from langchain.sql_database import SQLDatabase
+
+from langchain_openai import ChatOpenAI
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain.agents import create_sql_agent
 
 
 # pass in API key
@@ -11,3 +15,9 @@ load_dotenv(dotenv_path=".env")
 API_KEY = os.environ.get("OPENAI_API_KEY")
 
 db = SQLDatabase.from_uri(database_uri=SQLALCHEMY_URL)
+llm = ChatOpenAI(model=GPT_MODEL, temperature=0, api_key=API_KEY)
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+agent = create_sql_agent(llm=llm, toolkit=toolkit, verbose=True, top_k=100)
+
+agent.invoke(input={"input": "How many users are there for 2023?"})
--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,5 @@ SERVICE_ACCOUNT_FILE = "gcp_service_account_key.json"
 SQLALCHEMY_URL = (
     f"bigquery://{PROJECT}/{DATASET}?credentials_path={SERVICE_ACCOUNT_FILE}"
 )
+
+GPT_MODEL = "gpt-3.5-turbo-1106"

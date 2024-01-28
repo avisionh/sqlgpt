4028e931f2a899d741a8f2b70dcfd19f386c6b93
avisionh <a_vision@hotmail.co.uk>
2024-01-28 22:44:43 +0000

    chore: Connect to BigQuery database
    This is so we can look at tables within here and start getting chatGPT to answer questions in here.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+from src.constants import SQLALCHEMY_URL
+
+from langchain.sql_database import SQLDatabase
+
+
 # pass in API key
 load_dotenv(dotenv_path=".env")
 API_KEY = os.environ.get("OPENAI_API_KEY")
+
+db = SQLDatabase.from_uri(database_uri=SQLALCHEMY_URL)
